b07386c4ac3f1d16f2b0a2dffcecc00ede899f44
Rahul Kumar <rahulkaitian@gmail.com>
2018-05-07 00:41:43 +0530

    Create WebScraping_MF_MoneyControl.py

---
+++ WebScraping_MF_MoneyControl.py
@@ -0,0 +1,16 @@
+#This file can be used to fetch data from moneycontrol.com give MF url as parameter to this file
+import urllib3
+import sys
+import requests as rq
+from bs4 import BeautifulSoup
+
+mf_url=sys.argv[1]
+print ('Fetching data from URL:',mf_url)
+page=rq.get(mf_url)
+soup = BeautifulSoup(page.text, 'html.parser')
+rank_lst=soup.find_all(class_='bl_14')
+rank_in_categ= str(rank_lst[0].contents[0]).replace('<b>','').replace('</b>','')
+mf_category=str(rank_lst[1].contents[0]).replace('<b>','').replace('</b>','')
+rank_str=rank_in_categ+' in '+mf_category
+print (rank_str)
+portFolio_lst=soup.find_all(class_='tblporhd')
